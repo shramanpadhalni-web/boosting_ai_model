5aed5a009dd754d673761109518f5b03625b006b
hpadhaln <hem.chandra@corestack.io>
2022-02-19 11:06:10 +0530

    initial commit

---
+++ boosting_ai_model/features/build_features.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Feature building
+"""
+
+# import project config.py
+import boosting_ai_model.config as cfg
---
+++ boosting_ai_model/visualization/visualize.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Visualization
+"""
+# import project config.py
+import boosting_ai_model.config as cfg
